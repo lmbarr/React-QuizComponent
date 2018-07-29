0b86c47e86810e4ba0c9d866d7374bd7a85f4313
luis <lmbrmiguelito@hotmail.com>
2018-07-29 17:09:40 -0500

    3rd module

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,7 +13,7 @@ class Quiz extends Component {
     render() {
         return (
             <div>
-                <QuizQuestion className='QuizQuestion' quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}>
+                <QuizQuestion className='QuizQuestion' quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
             </div>
         )
     }
